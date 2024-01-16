61243acfad2d82e461ac5b484edf159e5d782431
pyl <a522723936@gmail.com>
2024-01-16 19:02:27 +0800

    audio组件已废弃，替换为uni.createInnerAudioContext

--- components/feng-parse/components/wxParseAudio.vue
+++ components/feng-parse/components/wxParseAudio.vue
@@ -1,28 +1,51 @@
 <template>
-	<!-- '<audio/>' 组件不再维护，建议使用能力更强的 'uni.createInnerAudioContext' 接口 有时间再改-->
-  <!--增加audio标签支持-->
-  <audio
-    :id="node.attr.id"
-    :class="node.classStr"
-    :style="node.styleStr"
-    :src="node.attr.src"
-    :loop="node.attr.loop"
-    :poster="node.attr.poster"
-    :name="node.attr.name"
-    :author="node.attr.author"
-    controls></audio>
+	<div>
+		<button @click="playAudio">播放</button>
+		<button @click="pauseAudio">暂停</button>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'wxParseAudio',
-  props: {
-    node: {
-      type: Object,
-      default() {
-        return {};
-      },
-    },
-  },
-};
+	export default {
+		data() {
+			return {
+				// 定义音频上下文
+				audioContext: null
+			};
+		},
+		props: {
+			node: {
+				type: Object,
+				default: () => ({})
+			}
+		},
+		mounted() {
+			this.createAudioContext();
+		},
+		methods: {
+			createAudioContext() {
+				// 创建 InnerAudioContext 实例
+				this.audioContext = uni.createInnerAudioContext();
+
+				// 设置音频属性
+				this.audioContext.src = this.node.attr.src;
+				this.audioContext.loop = this.node.attr.loop;
+				// 其他需要的属性...
+
+				// 开始播放（根据需要调用）
+				this.audioContext.play();
+			},
+			playAudio() {
+				this.audioContext.play();
+			},
+			pauseAudio() {
+				this.audioContext.pause();
+			}
+		},
+		// 当组件销毁时停止播放并释放资源
+		beforeDestroy() {
+			this.audioContext.stop();
+			this.audioContext.destroy();
+		}
+	};
 </script>
