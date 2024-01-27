e907664a23fb34e784512e93ccab81a17cb3c986
pyl <a522723936@gmail.com>
2024-01-27 20:50:04 +0800

    add province filter

--- pages/list/dropdown-setting.js
+++ pages/list/dropdown-setting.js
@@ -55,6 +55,115 @@ const infoTypeOptions = [{
 	},
 ]
 
+const provinces = [{
+		label: '国家',
+		value: '国家',
+	}, {
+		label: '安徽',
+		value: '安徽',
+	},
+	{
+		label: '北京',
+		value: '北京',
+	}, {
+		label: '重庆',
+		value: '重庆',
+	}, {
+		label: '福建',
+		value: '福建',
+	}, {
+		label: '广东',
+		value: '广东',
+	}, {
+		label: '甘肃',
+		value: '甘肃',
+	}, {
+		label: '广西',
+		value: '广西',
+	}, {
+		label: '贵州',
+		value: '贵州',
+	}, {
+		label: '河北',
+		value: '河北',
+	}, {
+		label: '湖北',
+		value: '湖北',
+	}, {
+		label: '黑龙江',
+		value: '黑龙江',
+	}, {
+		label: '河南',
+		value: '河南',
+	}, {
+		label: '海南',
+		value: '海南',
+	}, {
+		label: '湖南',
+		value: '湖南',
+	}, {
+		label: '吉林',
+		value: '吉林',
+	}, {
+		label: '江苏',
+		value: '江苏',
+	}, {
+		label: '江西',
+		value: '江西',
+	}, {
+		label: '辽宁',
+		value: '辽宁',
+	}, {
+		label: '内蒙古',
+		value: '内蒙古',
+	}, {
+		label: '宁夏',
+		value: '宁夏',
+	}, {
+		label: '青海',
+		value: '青海',
+	}, {
+		label: '四川',
+		value: '四川',
+	}, {
+		label: '山东',
+		value: '山东',
+	}, {
+		label: '上海',
+		value: '上海',
+	}, {
+		label: '山西',
+		value: '山西',
+	}, {
+		label: '陕西',
+		value: '陕西',
+	}, {
+		label: '天津',
+		value: '天津',
+	}, {
+		label: '西藏',
+		value: '西藏',
+	}, {
+		label: '新疆',
+		value: '新疆',
+	}, {
+		label: '云南',
+		value: '云南',
+	}, {
+		label: '浙江',
+		value: '浙江',
+	}, {
+		label: '香港',
+		value: '香港',
+	}, {
+		label: '澳门',
+		value: '澳门',
+	}, {
+		label: '台湾',
+		value: '台湾',
+	},
+]
+
 export const DropdownMenu = [{
 	title: '分类筛选',
 	type: 'filter',
@@ -68,5 +177,10 @@ export const DropdownMenu = [{
 		type: 'checkbox',
 		value: [],
 		options: infoTypeOptions
+	}, {
+		title: '省份',
+		type: 'checkbox',
+		value: [],
+		options: provinces
 	}, ]
 }]
